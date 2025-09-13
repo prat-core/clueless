6ecafd7c4555632223613fb7ee9c691fe0a40175
Advaith Karthikeyan <advaith334@gmail.com>
2025-09-13 14:29:26 -0400

    Add functions for user input

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,12 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 cors = flask_cors.CORS(app)
 
-# webscraping function
-@app.route('/webscrape', methods=['GET'])
-def webscrape(url):
+# flask API routes
+@app.route('/user_text_input', methods=['GET'])
+def user_text_input():
+    continue
+
+def user_speech_input():
     continue
 
 if __name__ == '__main__':
